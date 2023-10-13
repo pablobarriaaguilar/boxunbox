1139503fb916a13e1cf8d353a1b5261a142f3e59
pablobarriaaguilar <pablobarria@ug.uchile.cl>
2023-10-13 09:16:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection.PortableExecutable;
+
+object BoxedData = "hola";
+
+if(BoxedData is int){
+    Console.WriteLine("es int");
+}
+if(BoxedData is string){
+    Console.WriteLine("es string");
+}
+
+List<object> lista = new List<object>();
+
+lista.Add(7);
+lista.Add(28);
+lista.Add(-1);
+lista.Add(true);
+lista.Add("chair");
+
+int suma = 0;
+foreach( var elemento in lista){
+    Console.WriteLine(elemento);
+
+    if(elemento is int){
+        suma = suma + (int)elemento;
+    }
+}
+
+Console.WriteLine(suma);
+
